f1c1b9741b599c8fa840baa3cfa29bbde3d07dd1
Coaxial <coaxial@coaxat.com>
2022-05-21 16:09:00 -0300

    Return only rootdir of the pattern to search

--- src/utils.py
+++ src/utils.py
@@ -6,41 +6,41 @@ from liveProgress import LiveProgress
 class Utils():
 
     @classmethod
-    def find_backups(cls, rootdir: str, stop_after_first_result: bool = False):
+    def find(cls, search_from: str, target: list, stop_after_first_result: bool = False, use_progress_bar: bool = True):
 
-        if not rootdir or not os.path.isdir(rootdir):
+        if not search_from or not os.path.isdir(search_from):
 
-            raise NotADirectoryError("Provided rootdir does not exist or is not a directory")   
+            raise NotADirectoryError("Provided search fir does not exist or is not a directory")   
 
-        backup_required_files = [ "Manifest.plist", "Info.plist", "Status.plist", "Manifest.db" ]
+        result = []
 
-        backups = {}
+        oswalk_ref = cls.create_oswalk_progress_bar_ref(search_from = search_from)
 
-        oswalk_ref = cls.create_oswalk_progress_bar_ref(rootdir = rootdir)
-
-        progress_total = list( oswalk_ref.keys() )[0]
+        progress_total = list(oswalk_ref.keys())[0]
         
-        dirs_list_in_rootdir = list( oswalk_ref[progress_total] )
+        dirs_list_in_search_from = list(oswalk_ref[progress_total])
                     
-        progress_bar = LiveProgress(live_progress_type = "bar", text = "Researching backups ...", progress_total = progress_total)
-        progress_bar.start()
+        progress_bar = LiveProgress(live_progress_type = "bar", text = "Researching ...", progress_total = progress_total)
 
-        # Start researching
-        for path, dirnames, filenames in os.walk(rootdir, followlinks = False):
+        number_target = len(target)
+
+        if use_progress_bar:
+            progress_bar.start()
+
+        for path, dirnames, filenames in os.walk(search_from, followlinks = False):
 
             count = 0
             do_break = False
 
-            for file in backup_required_files: 
+            for file in target: 
 
                 if file in filenames:
                     count += 1
 
-                # When found a backup folder
-                if count == 4:
+                # When one or all pattern in a folder
+                if count == number_target:
 
-                    udid = path.split('/')[-1]
-                    backups[udid] = path.replace('/' + udid, '')
+                    result.append(path)
 
                     if stop_after_first_result:
                         do_break = True
@@ -48,28 +48,31 @@ class Utils():
             if do_break:
                 break
 
-            if path in dirs_list_in_rootdir:
+            if path in dirs_list_in_search_from:
                 progress_bar.current_progress += 1                
           
         progress_bar.current_progress = progress_total
-        progress_bar.join()
+        
+        if use_progress_bar: 
+            progress_bar.join()
 
-        return backups
+        return result
+
 
 
     @staticmethod
-    def create_oswalk_progress_bar_ref(rootdir: str):
+    def create_oswalk_progress_bar_ref(search_from: str):
 
         dirs_list = []
         dct = {}
 
         number_dirs = 0
 
-        for dir in os.listdir(rootdir):
+        for dir in os.listdir(search_from):
 
-            if os.path.isdir(os.path.join(rootdir, dir)):
+            if os.path.isdir(os.path.join(search_from, dir)):
 
-                dirs_list.append(os.path.join(rootdir, dir))
+                dirs_list.append(os.path.join(search_from, dir))
                 number_dirs += 1
 
         dct[number_dirs] = dirs_list
@@ -95,13 +98,19 @@ class Utils():
 
 
     @classmethod
-    def db_execute(self, db_file: str, query: str):
+    def db_execute(self, db_file: str, query: str, mode_row: bool = True, fetchall: bool = True):
         
         try:
             db = sqlite3.connect(db_file)
-            db.row_factory = sqlite3.Row
 
-            result = db.cursor().execute(query).fetchall()
+            if mode_row:
+                db.row_factory = sqlite3.Row
+
+            if fetchall:
+                result = db.cursor().execute(query).fetchall()
+
+            else:
+                result = db.cursor().execute(query).fetchone()
             
             return result
 
